e7efcae2082aaaa32520b04331eb23cdfd656fa5
Khalil Albattashi <albattashi.ka@gmail.com>
2021-03-29 21:08:37 -0500

    myclt function creates a histogram for a sample size n with lower limit and upper limit b

---
+++ R/myclt.R
@@ -0,0 +1,22 @@
+#' @title myclt function
+#'
+#' @param n sample size
+#' @param iter iterations
+#' @param a = lower limit of the uniform distribution
+#' @param b = upper limit of the uniform distribution
+#'
+#'
+#' @return a histogram
+#' @export
+#'
+#' @examples myclt(n = 4, iter = 1000, a = 0, b =  5)
+#'
+myclt=function(n,iter,a=0,b=5){
+  y=runif(n*iter,a,b)
+  data=matrix(y,nr=n,nc=iter,byrow=TRUE)
+  sm=apply(data,2,sum)
+  h=hist(sm,plot=FALSE)
+  hist(sm,col=rainbow(length(h$mids)),freq=FALSE,main="Distribution of the sum of uniforms")
+  curve(dnorm(x,mean=n*(a+b)/2,sd=sqrt(n*(b-a)^2/12)),add=TRUE,lwd=2,col="Blue")
+  sm
+}
